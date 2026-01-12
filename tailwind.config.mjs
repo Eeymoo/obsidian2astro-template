deeac8b5fe96f3f541f3302a492686a3fd737df6
Eeymoo <eeymoo@xmail.fun>
2026-01-12 19:44:08 +0800

    feat: 更新样式和布局，集成 Tailwind CSS 以增强视觉效果

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#8080f7',
+				secondary: '#F7F780',
+			},
+		},
 	},
 	plugins: [],
 }
