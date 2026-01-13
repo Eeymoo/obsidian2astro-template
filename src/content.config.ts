79abf4ca41039b9ce412dc5403454fbf934cc643
Eeymoo <eeymoo@xmail.fun>
2026-01-13 10:25:02 +0800

    feat: 更新博客链接和添加文章页面，优化布局和样式

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,12 +8,13 @@ const blog = defineCollection({
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
-			// description: z.string(),
+			// description: z.string().optional(),
 			// Transform string to Date object
 			// pubDate: z.coerce.date(),
 			date:  z.coerce.date(),
 			updated: z.coerce.date().optional(),
 			heroImage: image().optional(),
+			// uri: z.string() // 强制要求文章必须包含 uri 字段，否则校验失败
 		}),
 });
 
