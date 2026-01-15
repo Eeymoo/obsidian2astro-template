ff51b2a90264b6c1291b1d7e7e0006c018fffb58
Eeymoo <eeymoo@xmail.fun>
2026-01-15 18:08:10 +0800

    feat: 添加 FooterLinks 组件并更新配置，支持动态链接渲染

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,130 @@
 import type { Config } from "./types";
 
 const config: Config = {
-    siteTitle: 'My Astro Site',
-    siteDescription: 'A description of my Astro site.',
-    siteUrl: 'https://www.example.com',
-    friendlyLink: [],
-}
+  siteTitle: "My Astro Site",
+  siteDescription: "A description of my Astro site.",
+  siteUrl: "https://www.example.com",
+  friendlyLink: [
+    {
+      name: "Jellyfin",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "Jellyfin 插件同步站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "Jellyfin 插件镜像站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "Jellyfin 镜像同步站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "插件镜像同步站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+        {
+      name: "Jellyfin",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "Jellyfin 插件同步站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "Jellyfin 插件镜像站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "Jellyfin 镜像同步站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "插件镜像同步站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+        {
+      name: "Jellyfin",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "Jellyfin 插件同步站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "Jellyfin 插件镜像站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "Jellyfin 镜像同步站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "插件镜像同步站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+        {
+      name: "Jellyfin",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "Jellyfin 插件同步站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "Jellyfin 插件镜像站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "Jellyfin 镜像同步站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+    {
+      name: "插件镜像同步站",
+      link: "https://jellyfin-cn.eeymoo.com/",
+      desc: "为中国大陆用户提供高速稳定的 Jellyfin 插件镜像服务",
+      img: "https://jellyfin-cn.eeymoo.com/assets/icon.png",
+    },
+  ],
+};
 export default config;
