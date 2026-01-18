d67c449b9584d44c79d6327f5ba36cc48959cd26
eeymoo <eeymoo@xmail.fun>
2026-01-18 14:03:02 +0800

    feat: 统一代码风格，调整引入语句和删除无用代码

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.eeymoo.com',
+  site: "https://blog.eeymoo.com",
   integrations: [mdx(), sitemap()],
 
   markdown: {
     shikiConfig: {
-      theme: 'monokai',
+      theme: "monokai",
       wrap: true,
     },
   },
